71c801be1a2d8a0d3a53c55baca54f6274be482a
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-02-01 15:19:44 -0700

    added username to config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { config } = require("vuepress-theme-hope");
 
 
-const GITHUB_USERNAME = 'REPLACE_ME' // TODO 
+const GITHUB_USERNAME = 'cmitchell5619' // TODO 
 
 
 module.exports = config({
